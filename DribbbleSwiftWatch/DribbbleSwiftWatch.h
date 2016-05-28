fb09ce15625f1b855a369d7e37f330e568ecc2bd
= <qwstnz@gmail.com>
2016-05-28 18:44:16 -0400

    WatchOS support

---
+++ DribbbleSwiftWatch/DribbbleSwiftWatch.h
@@ -0,0 +1,19 @@
+//
+//  DribbbleSwiftWatch.h
+//  DribbbleSwiftWatch
+//
+//  Created by George on 2016-05-28.
+//  Copyright © 2016 George. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for DribbbleSwiftWatch.
+FOUNDATION_EXPORT double DribbbleSwiftWatchVersionNumber;
+
+//! Project version string for DribbbleSwiftWatch.
+FOUNDATION_EXPORT const unsigned char DribbbleSwiftWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DribbbleSwiftWatch/PublicHeader.h>
+
+
